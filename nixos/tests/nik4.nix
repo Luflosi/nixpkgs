aee642509cd6b58d96e2f397b4ad2f402d0f5025
Luflosi <luflosi@luflosi.de>
2026-01-09 16:49:43 +0100

    Don't use the --url option
    Nik5 does not support it.

--- nixos/tests/nik4.nix
+++ nixos/tests/nik4.nix
@@ -60,7 +60,7 @@ in
       machine.succeed("sudo -u osmuser psql -d gis -f '${pkgs.openstreetmap-carto}/functions.sql'")
 
     with subtest("Execute nik4.py"):
-      machine.succeed("sudo -u osmuser nik4.py --url 'https://www.openstreetmap.org/#map=17/42.506650/1.525828' --ppi 300 -a 4 '${pkgs.openstreetmap-carto}/mapnik.xml' /tmp/map.png >&2")
+      machine.succeed("sudo -u osmuser nik4.py -z 17 -c 1.525828 42.506650 --ppi 300 -a 4 '${pkgs.openstreetmap-carto}/mapnik.xml' /tmp/map.png >&2")
       machine.copy_from_vm("/tmp/map.png")
       import os
       image_size = os.stat(machine.out_dir / "map.png").st_size
